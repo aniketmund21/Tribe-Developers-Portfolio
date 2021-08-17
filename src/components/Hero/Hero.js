9490193388428c5828bd1aa37d34760f57701ee5
aniketmund21 <aniiketmund53@gmail.com>
2021-08-17 12:49:37 +0530

    first commit

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import Button from '../../styles/GlobalComponents/Button';
+import { LeftSection } from './HeroStyles';
+
+const Hero = (props) => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome to<br />
+        Tribe Developers
+
+      </SectionTitle>
+      <SectionText>
+        Tribe Developers provide App/Website Development Services for Businesses.
+      </SectionText>
+    </LeftSection>
+  </Section>
+);
+
+export default Hero;
